d52d494cf351e6cf8adbf0d427bfb78881db3a8a
kAmIkAdzE9 <gmtfkl@gmail.com>
2021-09-29 23:55:27 +0300

    Created empty project
    Created empty project

---
+++ InCamp_Test_Task/InCamp_Test_Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InCamp_Test_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
