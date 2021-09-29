63462ebebd8d08d182ce782a426cf34d85350c5c
kAmIkAdzE9 <gmtfkl@gmail.com>
2021-09-30 00:07:08 +0300

    Updated class Employee

--- InCamp_Test_Task/InCamp_Test_Task/Employee.cs
+++ InCamp_Test_Task/InCamp_Test_Task/Employee.cs
@@ -8,5 +8,30 @@ namespace InCamp_Test_Task
 {
     class Employee
     {
+        string name;
+        string date;
+        string hours;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getDate()
+        {
+            return date;
+        }
+
+        public string getHours()
+        {
+            return hours;
+        }
+
+        public Employee(string name, string date, string hours)
+        {
+            this.name = name;
+            this.date = date;
+            this.hours = hours;
+        }
     }
 }
